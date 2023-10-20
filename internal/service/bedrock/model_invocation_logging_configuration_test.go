55ce4067ef2c1200f2e516441f3117a57a7dfca6
Richard Weerasinghe <richard@skyscrapr.io>
2023-10-20 14:12:18 +1300

    more complete test

--- internal/service/bedrock/model_invocation_logging_configuration_test.go
+++ internal/service/bedrock/model_invocation_logging_configuration_test.go
@@ -88,11 +88,71 @@ resource "aws_s3_bucket_policy" "bedrock_logging" {
 EOF
 }
 
+resource "aws_cloudwatch_log_group" "bedrock_logging" {
+  name = "Bedrock-%[1]s"
+}
+
+resource "aws_iam_role" "bedrock_logging" {
+  name = "bedrock_logging-%[1]s"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "bedrock.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "aws:SourceAccount": "${data.aws_caller_identity.current.account_id}"
+        },
+        "ArnLike": {
+          "aws:SourceArn": "arn:${data.aws_partition.current.partition}:bedrock:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+        }
+      }
+    }
+  ]
+}  
+EOF
+}
+
+resource "aws_iam_policy" "bedrock_logging" {
+  name        = "bedrock_logging_%[1]s"
+  path        = "/"
+  description = "BedrockCloudWatchPolicy"
+
+  policy = jsonencode({
+      "Version": "2012-10-17", 
+      "Statement": [ 
+          {
+              "Effect": "Allow", 
+              "Action": [ 
+                  "logs:CreateLogStream", 
+                  "logs:PutLogEvents" 
+              ], 
+              "Resource": "${aws_cloudwatch_log_group.bedrock_logging.arn}:log-stream:aws/bedrock/modelinvocations" 
+           } 
+      ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "bedrock_logging" {
+  role       = aws_iam_role.bedrock_logging.name
+  policy_arn = aws_iam_policy.bedrock_logging.arn
+}
+
 resource "aws_bedrock_model_invocation_logging_configuration" "test" {
   logging_config {
     embedding_data_delivery_enabled = true
     image_data_delivery_enabled     = true
     text_data_delivery_enabled      = true
+    cloud_watch_config {
+      log_group_name = aws_cloudwatch_log_group.bedrock_logging.name
+      role_arn = aws_iam_role.bedrock_logging.arn
+    }
     s3_config {
       bucket_name = aws_s3_bucket.bedrock_logging.id
       key_prefix  = "bedrock"
